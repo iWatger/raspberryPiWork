e4a5cdcd980e54efd0157b175643200bd8fed648
iWatger <ethanfox646@icloud.com>
2020-07-26 14:52:30 -0400

    Create electric_car.py

---
+++ electric_car.py
@@ -0,0 +1,35 @@
+#Ethan Fox
+#electric_car.py
+class Car():
+     """A simple attempt to represent a car."""
+
+     def __init__(self, make, model, year):
+         self.make = make
+         self.model = model
+         self.year = year
+         self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + "miles on it.")
+
+    def update_odometer(self, milage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back anodometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspect of a car, specific to electric vehicles"""
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model y', 2019)
+print(my_tesla.get_descriptive_name())
